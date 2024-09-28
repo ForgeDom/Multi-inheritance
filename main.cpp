de194acd03d63a6be27a4ad3c360fd5603141d6e
ForgeDom <maxstill2006@gmail.com>
2024-09-28 16:55:56 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+
 using namespace std;
 
+class Square {
+protected:
+    int side;
+public:
+    Square(int s) : side(s) {}
+
+    int getSide() const {
+        return side;
+    }
+
+    double getArea() const {
+        return side * side;
+    }
+};
+
+class Circle {
+protected:
+    int radius;
+public:
+    Circle(int r) : radius(r) {}
+
+    int getRadius() const {
+        return radius;
+    }
+
+    double getArea() const {
+        return 3.14 * radius * radius;
+    }
+};
+
+class CircleInSquare : public Square, public Circle {
+public:
+    CircleInSquare(int side) : Square(side), Circle(side / 2) {}
+
+    void Show() const {
+        cout << "Square side: " << getSide() << endl;
+        cout << "Circle radius: " << getRadius() << endl;
+        cout << "Square area: " << Square::getArea() << endl;
+        cout << "Circle area: " << Circle::getArea() << endl;
+    }
+};
+
 int main() {
-	return 0;
+    CircleInSquare size(10);
+    size.Show();
+    return 0;
 }
+//Images\background.png

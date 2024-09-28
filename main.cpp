fe0766b01434fdad858b4e9dd2499099ab8a9d1f
ForgeDom <maxstill2006@gmail.com>
2024-09-28 17:47:17 +0300

    +

--- main.cpp
+++ main.cpp
@@ -3,61 +3,62 @@ using namespace std;
 
 class Wheels {
 protected:
-	int wheels_amount;
+    int wheels_amount;
 public:
-	Wheels(int amount) : wheels_amount(amount) {};
-	virtual ~Wheels() {
-		cout << "Wheel destructor called" << endl;
-	}
-	void wheelRotate() const {
-		cout << "Your car has " << wheels_amount << " wheels" << endl;
-	}
+    Wheels(int amount) : wheels_amount(amount) {};
+    virtual ~Wheels() {
+        cout << "Wheel destructor called" << endl;
+    }
+    virtual void print() const {
+        cout << "Your car has " << wheels_amount << " wheels." << endl;
+    }
 };
 
 class Engine {
 protected:
-	int cylinders;
+    int cylinders;
 public:
-	Engine(int v_amount) : cylinders(v_amount) {}
-	virtual ~Engine() {
-		cout << "Engine destructor called" << endl;
-	}
-	void enginePower() const{
-		cout << "Your car is rather powerful, it has " << cylinders << " cylinders" << endl;
-	}
+    Engine(int v_amount) : cylinders(v_amount) {}
+    virtual ~Engine() {
+        cout << "Engine destructor called" << endl;
+    }
+    virtual void print() const {
+        cout << "Your car is rather powerful, it has " << cylinders << " cylinders." << endl;
+    }
 };
 
 class Doors {
 protected:
-	int door_count;
+    int door_count;
 public:
-	Doors(int doors) : door_count(doors) {};
-	virtual ~Doors() {
-		cout << "Doors destructor called" << endl;
-	}
-	void openingDoors() {
-		cout << "Your car has " << door_count << ", but only " << door_count - 1 << " can open(" << endl;
-	}
+    Doors(int doors) : door_count(doors) {};
+    virtual ~Doors() {
+        cout << "Doors destructor called" << endl;
+    }
+    virtual void print() const { 
+        cout << "Your car has " << door_count << ", but only " << door_count - 1 << " can open." << endl;
+    }
 };
 
 class Car : virtual public Wheels, virtual public Engine, virtual public Doors {
 protected:
-	string car_model;
+    string car_model;
 public:
-	Car(int wheels, int cylinders, int doors, string model) : Wheels(wheels), Engine(cylinders), Doors(doors), car_model(model){}
+    Car(int wheels, int cylinders, int doors, string model)
+        : Wheels(wheels), Engine(cylinders), Doors(doors), car_model(model) {}
 
-	void showcar() {
-		cout << "Your car is:" << car_model << endl;
-		wheelRotate();
-		enginePower();
-		openingDoors();
-	}
+    void print() const override {
+        cout << "Your car is: " << car_model << endl;
+        Wheels::print();
+        Engine::print();
+        Doors::print();
+    }
 };
 
 int main() {
-	Car*  mycar = new Car(4, 16, 2, "Toyota Mark-2");
-	mycar->showcar();
-	cout << endl << "<<------------------------------------------------>>" << endl << endl;;
-	delete mycar;
-	return 0;
+    Car* mycar = new Car(4, 16, 2, "Toyota Mark-2");
+    mycar->print();
+    cout << endl << "<<------------------------------------------------>>" << endl << endl;
+    delete mycar;
+    return 0;
 }
